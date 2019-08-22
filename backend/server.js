c94c8693d535ffd86ac58e0715610811fdb67348
Sinduja Jagadeesan <jagadeesansinduja@johndeere.com>
2019-08-21 19:54:18 -0500

    chore: add gitignore

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const Data = require('./data');
 const DomainData = require('./domain-data');
 
 const API_PORT = 3001;
@@ -11,18 +10,19 @@ const app = express();
 app.use(cors());
 const router = express.Router();
 
-// this is our MongoDB database
 const dbRoute =
-  'mongodb+srv://domainUser:domainPassword@domaincluster-hzl4n.mongodb.net/domainTable?retryWrites=true&w=majority';
-  
-// connects our back end code with the database
-mongoose.connect(dbRoute, { useNewUrlParser: true });
+  'mongodb+srv://domainUser:domainPassword@domaincluster-hzl4n.mongodb.net/domainTable?authSource=admin';
+
+mongoose.connect(dbRoute, { useNewUrlParser: true }).then(() => {
+  console.log("Connected to Database");
+}).catch((err) => {
+  console.log("Not Connected to Database ERROR! ", err);
+});
 
 let db = mongoose.connection;
 
 db.once('open', () => console.log('connected to the database'));
 
-// checks if connection with the database is successful
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 db.on('connected', function () {
@@ -33,20 +33,9 @@ db.on('disconnected', function () {
 console.log('Mongoose disconnected');
 });
 
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
-
-// this is our get method
-// this method fetches all available data in our database
-router.get('/getData', (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
 
 router.get('/getDomainData', (req, res) => {
   DomainData.find((err, data) => {
@@ -69,28 +58,6 @@ router.post('/addDomainName', (req, res) => {
   });
 });
 
-router.post('/putData', (req, res) => {
-  let data = new Data();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: 'INVALID INPUTS',
-    });
-  }
-  data.message = message;
-  data.id = id;
-  console.log(data)
-  data.save((err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// append /api for our http requests
 app.use('/api', router);
 
-// launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
